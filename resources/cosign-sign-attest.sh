2c8d916b6acec03fc7d3ccb6dfa044a5c5064455
David Peraza <dperaza@redhat.com>
2025-10-31 17:19:38 -0400

    Update cosign-sign-attest.sh
    bypass alias

--- resources/cosign-sign-attest.sh
+++ resources/cosign-sign-attest.sh
@@ -56,7 +56,7 @@ function cosign-cmd() {
     # (There are also numerous other ways to provide the secret key to cosign.)
     COSIGN_PASSWORD=$(base64d "$COSIGN_SECRET_PASSWORD") \
     COSIGN_KEY=$(base64d "$COSIGN_SECRET_KEY") \
-        cosign "$cmd" -y --key=env://COSIGN_KEY $REKOR_OPT "${opts[@]}" "$FULL_IMAGE_REF"
+        \cosign "$cmd" -y --key=env://COSIGN_KEY $REKOR_OPT "${opts[@]}" "$FULL_IMAGE_REF"
 }
 
 # Generates data for an attestation predicate
